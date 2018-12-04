1bd28989961673a703fedfc5bfd4f047296d6e04
Prince Avila_b <43385846+TennisSprinterkb@users.noreply.github.com>
2018-12-04 16:27:55 -0500

    picturesupdate

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TriviaGame
 Trivia question game
+Kb Trivia takes different areas of topic, and quizzes you on the it. This trivia style game is on the most amazing sport Track and Field. 
+
+Being one of the oldest olympic sport and one even before then, it has made a gret debut back with rising stars.
+Will you be able to Sprint your way to the finishling in this series of track questions?
+
+Basic Instructions
+A trivia game that shows only one question until the player answers it or their time runs out.
+If the player selects the correct answer, a screen congratulating them for choosing the right option. After a few seconds, display the next question -- no input required by user.
+
+The scenario is similar for wrong answers and time-outs.
+
+If the player runs out of time, the player will have a screen that says time's up and display the correct answer. Wait a few seconds, then the next quesiton will show.
+
+If the player chooses the wrong answer, the player will see "wrong option" or "incorrect" and then the correct answer will display. Wait a few seconds, then the next question will show.
+
+On the final screen,will show the number of correct answers, incorrect answers, and an option to restart the game.
---
+++ assets/javascript/app.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+// Questions the user will have to answer
+var trackQuestion = [{
+    question: "Who holds the World Record in the 100 metres?",
+    answers:["Christian Coleman", "Michael Rodgers", "Usain Bolt", "Justin Gatlin"],
+    correctOne:"Usain Bolt",
+    //image
+},{
+    question: "Which Female holds the World Record in the 100 metres?",
+    answers: ["Florence Griffith-Joyner", "Carmelita Jeter", "Shelly-Ann Fraser-Pryce", "Marion Jones"],
+    correctOne: "Florence Griffith-Joyner",
+    //image
+},{
+    question: "What Female athlete hold the World Record in the 200 metres?",
+    answers: ["Marion Jones", "Florence Griffith-Jones", "Allyson Felix", "Dafne Schippers"],
+    correctOne: "Florence Griffith-Joyner",
+    //image
+},{
+    question: " What Female holds the World Record in the 400 metres?",
+    answers: ["Marita Koch", "Sanya Richard-Ross", "Shaunae Miller-Uibo", "Cathy Freeman"],
+    correctOne: "Marita Koch",
+    //image
+},{
+    question: "Who has the World Record in the 200 metres?",  
+    answers: ["Yohan Blake", "Michael Jonson", "Walter Dix","Usain Bolt"],
+    correctOne: "Usain Bolt",
+    //image
+},{
+    question: "Who has the World Record in the 400 metres?",  
+    answers: ["Jeremy Wariner", "Michael Johnson", "Wayde van Niekerk", "LaShawn Merritt"],
+    correctOne: "Wayde van Niekerk",
+    //image
+},{
+    question: " What team holds the Word Record in the 4x100m Relay?",    
+    answers: ["Jamaica", "USA", "Germany", "Great Britain & N.I."],
+    correctOne: "Jamaica",
+    //image
+},{
+    question: "Which team holds the World Record in the 4x400m Relay?",    
+    answers: ["Great Britain & N.I.", "Bahamas", "USA", "Jamaica"],
+    correctOne: "USA",
+    //image
+},{
+    question: "Which Track & Field Athlete recieved the highest paid contract?",
+    answers: ["Allyson Felix", "Sydney McLaughlin", "Jenny Simpson", "Christian Coleman"],
+    correctOne: "Sydney McLaughlin",
+    //image
+},{
+    quesiton: "Which Female team has the World Record in the 4x100m Relay?",
+    answers: ["USA", "Jamaica", "Russia", "Germany"],
+    correctOne: "USA",
+    //image
+}
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+})
